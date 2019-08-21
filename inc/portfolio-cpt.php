4aad5044ef80c6ede4c306d387c02ac3ef91b2a7
Travis Holt <travisgholtsr@gmail.com>
2019-08-21 07:50:45 -0700

    Changed dashicons
    changed dashicons for testomonials and portolio cpt.

--- inc/portfolio-cpt.php
+++ inc/portfolio-cpt.php
@@ -45,6 +45,7 @@ function portfolio_post_type() {
 		'show_ui'               => true,
 		'show_in_menu'          => true,
 		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-art',
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
--- inc/testimonials-cpt.php
+++ inc/testimonials-cpt.php
@@ -1,49 +1,50 @@
-<?php 
-// Post Type: Testimonial
+<?php
+// Post Type: Testimonials
 // Author: Travis Holt
 // Package: @two-four
-
-function testimonials_cpt() {
+function testimonial_post_type() {
 
 	$labels = array(
-		'name'                  => 'Testimonials',
-		'singular_name'         => 'Testimonial',
-		'menu_name'             => 'Testimonials',
-		'name_admin_bar'        => 'Testimonial',
-		'archives'              => 'Testimonial Archives',
-		'attributes'            => 'Testimonial Attributes',
-		'parent_item_colon'     => 'Parent Testimonial:',
-		'all_items'             => 'All Testimonials',
-		'add_new_item'          => 'Add New Testimonial',
-		'add_new'               => 'Add New',
-		'new_item'              => 'New Testimonial',
-		'edit_item'             => 'Edit Testimonial',
-		'update_item'           => 'Update Testimonial',
-		'view_item'             => 'View Testimonial',
-		'view_items'            => 'View Testimonials',
-		'search_items'          => 'Search Testimonial',
-		'not_found'             => 'Not found',
-		'not_found_in_trash'    => 'Not found in Trash',
-		'featured_image'        => 'Featured Image',
-		'set_featured_image'    => 'Set featured image',
-		'remove_featured_image' => 'Remove featured image',
-		'use_featured_image'    => 'Use as featured image',
-		'insert_into_item'      => 'Insert into Testimonial',
-		'uploaded_to_this_item' => 'Uploaded to this Testimonial',
-		'items_list'            => 'Testimonials list',
-		'items_list_navigation' => 'Testimonials list navigation',
-		'filter_items_list'     => 'Filter Testimonials list',
+		'name'                  => _x( 'Testimonials', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Testimonial', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Testimonials', 'text_domain' ),
+		'name_admin_bar'        => __( 'Testimonial', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
 	);
 	$args = array(
-		'label'                 => 'Testimonial',
-		'description'           => 'Client testimonials custom post type with a beautiful UI/UX that is easy to use.',
+		'label'                 => __( 'Testimonial', 'text_domain' ),
+		'description'           => __( 'Testimonial information page.', 'text_domain' ),
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor' ),
-		'hierarchical'          => true,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+		'taxonomies'            => array( 'category' ),
+		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
 		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-awards',
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
@@ -55,4 +56,4 @@ function testimonials_cpt() {
 	register_post_type( 'testimonials', $args );
 
 }
-add_action( 'init', 'testimonials_cpt', 0 );
+add_action( 'init', 'testimonial_post_type', 0 );
